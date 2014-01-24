fa3020ea4a029e2b93781500561e562b87768f77
Parminder Nayyar Chhatwal <parminder.n.chhatwal@gmail.com>
2014-01-24 13:17:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicnicOnRails::Application.config.secret_token = '6233c3404543540bd7942884640c6099de18b0e1ea6144330256bb2069aa510e9f1ed4689a8f179be0b9a156bde223922ff9dc24ef0c8ffd3e023dcc37b3cf71'
